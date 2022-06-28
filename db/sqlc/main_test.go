a6335101bb0cd74981e6e73321eae5ace5fbf62f
csdengh <csdengh@outlook.com>
2022-06-28 15:34:20 +0800

    workflow fix error

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -10,12 +10,14 @@ import (
 )
 
 const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@192.168.66.16:5432/simple_bank?sslmode=disable"
+	dbDriver       = "postgres"
+	dbSource       = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	dbSourcegithub = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
 var testDb *sql.DB
+
 func TestMain(m *testing.M) {
 	var err error
 	testDb, err = sql.Open(dbDriver, dbSource)
